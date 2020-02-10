8b813066e830ac5f1a5517f5f948cf1c7f0f2496
IsaacUP <183411@ids.upchiapas.edu.mx>
2020-02-10 11:11:06 -0600

    Get/Post

--- Login/urls.py
+++ Login/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path, re_path
 from django.conf.urls import include
 from django.contrib.auth.models import User
-# from rest_framework import routers, serilizers, viewsets
 from Login.views import CustonAuthToken
 
+from Login import views
+
 urlpatterns = [
-    re_path(r'^', CustonAuthToken.as_view()),
-    #Hola soy Isaac
+    re_path(r'login/$', CustonAuthToken.as_view()),
+    
+    re_path(r'Login_lista2/$', views.ExampleList2.as_view()),
+    # re_path(r'Login_detail2/(?P<id>\d+)/$',views.ExampleDetail2.as_view())
 ]
